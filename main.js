bb19271d6dbe995c2c79221654e20661395a0457
NKWaters <82752247+NKWaters@users.noreply.github.com>
2021-12-13 16:42:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function preload() {
+
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet is Initialized')
+}
+
+function gotPoses(results) {
+    console.log(results); 
+    console.log("nose X" = results[0].pose.nose.x);
+    console.log("nose Y" = results[0].pose.nose.y);
+}
+function draw() {
+    
+}
